c46c198fec6b887185a872072f1764189bba749b
Alfredo Torres <alfredotorres.niu@gmail.com>
2023-02-12 13:43:05 -0600

    almacenamiento de datos en local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Formulario from './components/Formulario'
 import ListadoPacientes from './components/ListadoPacientes'
@@ -9,12 +9,30 @@ function App() {
   // para editar informacion
   const [paciente, setPaciente] = useState({});
 
+  //Guadar el local Storage
+
+  //Obtener lo que hay en local storate
+  useEffect(() => {
+    const obtenerLS = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? []  // Si no hay, lo pone como arreglo
+      setPacientes(pacientesLS)
+    }
+    obtenerLS();
+  },[])
+
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify(pacientes))
+  },[pacientes])
+
   // Eliminar paciente 
   const eliminarPaciente = (id) => {
 
     const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id)
     setPacientes(pacientesActualizados)
   }
+
+  
+
   return (
     <div className="container mx-auto mt-20">
      <Header/>
